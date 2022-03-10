b4ed31b96d97de5b09b00cdd945129219055d493
dongbq91 <dongbq91@gmail.com>
2022-03-10 15:38:35 +0700

    Sua ten file product

--- src/main/java/com/example/bebenshop/repository/ProductImageRepository.java
+++ src/main/java/com/example/bebenshop/repository/ProductImageRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductImageRepositopy extends JpaRepository<ProductImageEntity, Long> {
+public interface ProductImageRepository extends JpaRepository<ProductImageEntity, Long> {
 }
