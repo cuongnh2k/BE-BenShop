25233a1f6ce4fa98984dd534f38c2c56602c5d2b
doquoctoan11 <doquoctoan1111997@gmail.com>
2022-03-10 15:58:07 +0700

    create service

---
+++ src/main/java/com/example/bebenshop/services/impl/OrderServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.bebenshop.services.impl;
+
+import com.example.bebenshop.services.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+}
