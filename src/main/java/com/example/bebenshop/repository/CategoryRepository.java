1d1f027b69ab87faf5d72b106f2c085f0f748c5f
cuongnh2k <cuongnh2k@gmail.com>
2022-03-14 13:51:41 +0700

    fix bug api delete category

--- src/main/java/com/example/bebenshop/repository/CategoryRepository.java
+++ src/main/java/com/example/bebenshop/repository/CategoryRepository.java
@@ -2,12 +2,14 @@ package com.example.bebenshop.repository;
 
 import com.example.bebenshop.entities.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
+    @Modifying
     @Query(nativeQuery = true
             , value = "DELETE FROM product_entity_categories WHERE categories_id = ?1 ")
     void deleteProductCategoryById(Long id);
