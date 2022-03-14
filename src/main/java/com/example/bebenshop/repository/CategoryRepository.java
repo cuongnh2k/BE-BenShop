d222863ca5e03dc6de7a06e9bbc4a7957d5daa7f
unknown <nmhahalo2000@gmail.com>
2022-03-14 17:51:15 +0700

    15-edit-category

--- src/main/java/com/example/bebenshop/repository/CategoryRepository.java
+++ src/main/java/com/example/bebenshop/repository/CategoryRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
