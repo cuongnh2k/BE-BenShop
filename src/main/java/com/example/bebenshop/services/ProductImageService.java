316b31529cdb1c7bfbc04f89f125eca474fcfc53
cuongnh2k <cuongnh2k@gmail.com>
2022-03-15 17:10:59 +0700

    refactor logic

---
+++ src/main/java/com/example/bebenshop/services/ProductImageService.java
@@ -0,0 +1,13 @@
+package com.example.bebenshop.services;
+
+import com.example.bebenshop.dto.produces.ProductProduceDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ProductImageService {
+
+    ProductProduceDto addProductImage(Long id, MultipartFile multipartFile) throws IOException;
+
+    void deleteProductImage(Long id);
+}
