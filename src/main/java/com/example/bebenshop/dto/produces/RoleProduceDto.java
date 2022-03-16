53884a8f265a5bbbc5496a052f3a11928fc4b935
unknown <nmhahalo2000@gmail.com>
2022-03-16 22:37:31 +0700

    update-25-create-register

---
+++ src/main/java/com/example/bebenshop/dto/produces/RoleProduceDto.java
@@ -0,0 +1,9 @@
+package com.example.bebenshop.dto.produces;
+
+import com.example.bebenshop.bases.BaseProduceDto;
+import com.example.bebenshop.enums.RoleEnum;
+
+public class RoleProduceDto extends BaseProduceDto<Long> {
+
+    private RoleEnum name;
+}
