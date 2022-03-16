2e33378d882c817eeb4b4b759e557aca916d12ed
doquoctoan11 <doquoctoan1111997@gmail.com>
2022-03-16 22:53:48 +0700

    delete

--- src/main/java/com/example/bebenshop/repository/ProductCommentrepsitory.java
+++ src/main/java/com/example/bebenshop/repository/ProductCommentrepsitory.java
@@ -2,9 +2,13 @@ package com.example.bebenshop.repository;
 
 import com.example.bebenshop.entities.ProductCommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductCommentrepsitory extends JpaRepository<ProductCommentEntity,Long> {
     boolean existsById(Long parentId);
+
+    @Query(nativeQuery = true,value = "DELETE FROM product_comment_entity where  id=?1")
+    void deleteProductComment(Long id);
 }
