274c07cc1e6bddd723d2d1e879c8e886fca51e55
dongbq91 <dongbq91@gmail.com>
2022-03-17 09:41:17 +0700

    create

--- src/main/java/com/example/bebenshop/services/impl/OrderNoteServiceImpl.java
+++ src/main/java/com/example/bebenshop/services/impl/OrderNoteServiceImpl.java
@@ -1,16 +1,22 @@
 package com.example.bebenshop.services.impl;
 
+import com.example.bebenshop.dto.consumes.OrderConsumeDto;
 import com.example.bebenshop.dto.consumes.OrderNoteConsumeDto;
+import com.example.bebenshop.dto.consumes.UserConsumeDto;
 import com.example.bebenshop.dto.produces.OrderNoteProduceDto;
 import com.example.bebenshop.entities.OrderEntity;
 import com.example.bebenshop.entities.OrderNoteEntity;
 import com.example.bebenshop.entities.UserEntity;
+import com.example.bebenshop.enums.RoleEnum;
 import com.example.bebenshop.exceptions.BadRequestException;
 import com.example.bebenshop.mapper.OrderMapper;
 import com.example.bebenshop.mapper.OrderNoteMapper;
+import com.example.bebenshop.mapper.RoleMapper;
 import com.example.bebenshop.repository.OrderNoteRepository;
 import com.example.bebenshop.repository.OrderRepository;
+import com.example.bebenshop.repository.RoleRepository;
 import com.example.bebenshop.services.OrderNoteService;
+import com.example.bebenshop.services.UserService;
 import com.example.bebenshop.util.ConvertUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,15 +32,20 @@ public class OrderNoteServiceImpl implements OrderNoteService {
     private final OrderRepository mOrderRepository;
     private final OrderNoteMapper mOrderNoteMapper;
     private final RowMapper rowMapper;
+    private final UserService mUserService;
+    private final RoleRepository roleRepository;
     @Override
     public OrderNoteProduceDto addOrderNote(Long id , OrderNoteConsumeDto orderNoteConsumeDto) {
 
         OrderEntity orderEntity = mOrderRepository.findByIdAndDeletedFlagFalse(id);
-
         if(orderEntity == null){
             throw new BadRequestException("No order exists: "+id);
         }
+//        UserEntity userEntity = (UserEntity) roleRepository.findByName(RoleEnum.ROLE_USER);
+//        orderEntity.setUser(userEntity);
+//
         OrderNoteEntity orderNoteEntity = orderNoteConsumeDto.toOrderNoteEntity();
+
         orderNoteEntity.setOrder(orderEntity);
         mOrderNoteRepository.save(orderNoteEntity);
         return mOrderNoteMapper.toOrderNoteProduceDto(orderNoteEntity);
