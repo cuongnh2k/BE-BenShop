6aba1200efd3b5aa594d0db785859272bd5ad345
doquoctoan11 <doquoctoan1111997@gmail.com>
2022-03-17 08:40:51 +0700

    33-create-api-delete-productcomment

--- src/main/java/com/example/bebenshop/repository/ProductCommentrepsitory.java
+++ src/main/java/com/example/bebenshop/repository/ProductCommentrepsitory.java
@@ -2,6 +2,7 @@ package com.example.bebenshop.repository;
 
 import com.example.bebenshop.entities.ProductCommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface ProductCommentrepsitory extends JpaRepository<ProductCommentEntity,Long> {
     boolean existsById(Long parentId);
 
+    @Modifying
     @Query(nativeQuery = true,value = "DELETE FROM product_comment_entity where  id=?1")
     void deleteProductComment(Long id);
 }
