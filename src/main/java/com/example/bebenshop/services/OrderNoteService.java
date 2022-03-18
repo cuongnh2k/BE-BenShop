b921801258834c306dab5b60e911b6af6eeaffcb
doquoctoan11 <doquoctoan1111997@gmail.com>
2022-03-18 08:47:11 +0700

    34-delete-order-note

--- src/main/java/com/example/bebenshop/services/OrderNoteService.java
+++ src/main/java/com/example/bebenshop/services/OrderNoteService.java
@@ -1,4 +1,5 @@
 package com.example.bebenshop.services;
 
 public interface OrderNoteService {
+    void deleteOderNoteById(Long id);
 }
--- src/main/java/com/example/bebenshop/services/impl/OrderNoteServiceImpl.java
+++ src/main/java/com/example/bebenshop/services/impl/OrderNoteServiceImpl.java
@@ -1,6 +1,12 @@
 package com.example.bebenshop.services.impl;
 
+import com.example.bebenshop.entities.OrderNoteEntity;
+import com.example.bebenshop.exceptions.BadRequestException;
+import com.example.bebenshop.exceptions.ForbiddenException;
+import com.example.bebenshop.mapper.OrderNoteMapper;
+import com.example.bebenshop.repository.OrderNoteRepository;
 import com.example.bebenshop.services.OrderNoteService;
+import com.example.bebenshop.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class OrderNoteServiceImpl implements OrderNoteService {
+    private final OrderNoteRepository mOrderNoteRepository;
+    private final OrderNoteMapper mOrderNoteMapper;
+    private final UserService mUserService;
+
+    @Override
+    public void deleteOderNoteById(Long id) {
+        OrderNoteEntity orderNoteEntity = mOrderNoteRepository.findById(id).orElse(null);
+        if (orderNoteEntity == null) {
+            throw new BadRequestException("Id " + id + " not does exits ");
+        }
+        if (orderNoteEntity.getOrder().getUser().getId() != mUserService.getCurrentUser().getId()) {
+            throw new ForbiddenException(" not authorithor delete order note");
+        }
+        mOrderNoteRepository.deleteOrderNoteById(id);
+    }
 }
