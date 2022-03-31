b3c1adefb4044d4ecda799aeae1d1bfb0f953cd0
dongbq91 <dongbq91@gmail.com>
2022-03-31 09:27:17 +0700

    create-get-commet

--- src/main/java/com/example/bebenshop/services/impl/ProductCommentServiceImpl.java
+++ src/main/java/com/example/bebenshop/services/impl/ProductCommentServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.bebenshop.services.impl;
 import com.example.bebenshop.dto.consumes.ProductCommentConsumeDto;
 import com.example.bebenshop.dto.produces.ProductCommentProduce1Dto;
 import com.example.bebenshop.dto.produces.ProductCommentProduceDto;
+import com.example.bebenshop.dto.produces.ProductProduceDto;
 import com.example.bebenshop.entities.ProductCommentEntity;
 import com.example.bebenshop.entities.ProductEntity;
 import com.example.bebenshop.entities.UserEntity;
@@ -10,7 +11,6 @@ import com.example.bebenshop.exceptions.BadRequestException;
 import com.example.bebenshop.exceptions.ForbiddenException;
 import com.example.bebenshop.mapper.ProductCommentMapper;
 import com.example.bebenshop.mapper.ProductMapper;
-import com.example.bebenshop.mapper.UserMapper;
 import com.example.bebenshop.repository.ProductCommentRepository;
 import com.example.bebenshop.repository.ProductRepository;
 import com.example.bebenshop.services.ProductCommentService;
@@ -30,8 +30,8 @@ public class ProductCommentServiceImpl implements ProductCommentService {
     private final ProductCommentMapper mProductCommentMapper;
     private final ProductRepository mProductRepository;
     private final UserService mUserService;
-    private final UserMapper mUserMapper;
-    private final ProductMapper mProductMapper;
+    private  final ProductMapper mProductMapper;
+
 
     @Override
     public ProductCommentProduceDto createProductComment(ProductCommentConsumeDto productCommentConsumeDto, Long id) {
@@ -77,10 +77,10 @@ public class ProductCommentServiceImpl implements ProductCommentService {
         mProductCommentRepository.deleteProductComment(id);
     }
 
-    public ProductCommentProduceDto toProductCommentProduceDto (ProductCommentEntity productCommentEntity) {
+    public ProductCommentProduceDto toProductCommentProduceDto(ProductCommentEntity productCommentEntity) {
         ProductCommentProduceDto productCommentProduceDto = mProductCommentMapper.toProductCommentProduceDto(productCommentEntity);
         List<ProductCommentProduceDto> productCommentProduceDtoList = mProductCommentRepository.findAll().stream()
-                .map(mProductCommentMapper:: toProductCommentProduceDto).collect(Collectors.toList());
+                .map(mProductCommentMapper::toProductCommentProduceDto).collect(Collectors.toList());
         productCommentProduceDtoList.stream().filter(o -> o.getParentId() == 0)
                 .map(o -> ProductCommentProduceDto.builder()
                         .id(o.getId())
@@ -100,38 +100,20 @@ public class ProductCommentServiceImpl implements ProductCommentService {
                                         .user(oo.getUser())
                                         .build()).collect(Collectors.toList()))
                         .build()).collect(Collectors.toList());
-                        return productCommentProduceDto;
+        return productCommentProduceDto;
     }
 
     @Override
     public ProductCommentProduceDto getCommentByProductId(Long id) {
         ProductEntity productEntity = mProductRepository.findByIdAndDeletedFlagFalse(id);
+
         if (productEntity == null) {
             throw new BadRequestException("Comment does not exist");
         }
-        ProductCommentEntity productCommentEntity = mProductCommentRepository.findById(id).orElse(null);
-//        List<ProductCommentProduceDto> productCommentProduceDtoList = mProductCommentRepository.findAll().stream()
-//                .map(mProductCommentMapper:: toProductCommentProduceDto).collect(Collectors.toList());
-//        productCommentProduceDtoList.stream().filter(o -> o.getParentId() == 0)
-//                .map(o -> ProductCommentProduceDto.builder()
-//                        .id(o.getId())
-//                        .createdDate(o.getCreatedDate())
-//                        .updatedDate(o.getUpdatedDate())
-//                        .content(o.getContent())
-//                        .parentId(o.getParentId())
-//                        .user(o.getUser())
-//                        .productComment1(productCommentProduceDtoList.stream()
-//                                .filter(oo -> o.getId() == oo.getParentId())
-//                                .map(oo -> ProductCommentProduce1Dto.builder()
-//                                        .id(oo.getId())
-//                                        .createdDate(oo.getCreatedDate())
-//                                        .updatedDate(oo.getUpdatedDate())
-//                                        .content(oo.getContent())
-//                                        .parentId(oo.getParentId())
-//                                        .user(oo.getUser())
-//                                        .build()).collect(Collectors.toList()))
-//                        .build()).collect(Collectors.toList());
 
-        return toProductCommentProduceDto(productCommentEntity);
+        List<ProductCommentEntity> productCommentEntityList = (List<ProductCommentEntity>) productEntity.getProductComments();
+        productCommentEntityList.stream().map(mProductCommentMapper::toProductCommentProduceDto).collect(Collectors.toList());
+
+        return toProductCommentProduceDto((ProductCommentEntity) productCommentEntityList);
     }
 }
